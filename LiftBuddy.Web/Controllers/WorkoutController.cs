67121374cdd1f8b4d9f3c1448fa4c34fa0d89809
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-19 16:05:00 -0400

    Added workout controller and repository

---
+++ LiftBuddy.Web/Controllers/WorkoutController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using LiftBuddy.Models;
+using LiftBuddy.Web.Models;
+using LiftBuddy.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LiftBuddy.Web.Controllers
+{
+    [Route("workouts")]
+    public class WorkoutController : Controller
+    {
+        private readonly ILogger<WorkoutController> _logger;
+        private readonly WorkoutRepository _repository;
+
+        public WorkoutController(ILogger<WorkoutController> logger, WorkoutRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+        
+        public IActionResult Index()
+        {
+            var workouts = _repository.GetAll();
+            return View(workouts);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("error")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LiftBuddy.Web/Repositories/WorkoutRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using LiftBuddy.Models;
+
+namespace LiftBuddy.Web.Repositories
+{
+    public class WorkoutRepository
+    {
+        public IList<Workout> GetAll()
+        {
+            var workouts = new List<Workout>();
+            for (var i = 0; i < 10; i++)
+            {
+                workouts.Add(new Workout
+                {
+                    Id = Guid.NewGuid(),
+                    PerformedAt = DateTime.UtcNow
+                });
+            }
+            return workouts;
+        }
+    }
+}
