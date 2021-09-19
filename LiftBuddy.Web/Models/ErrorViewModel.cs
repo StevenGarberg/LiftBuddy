c2eab5b723fd21f9a69727e0ccc6a248bcc9b9f0
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-19 15:51:17 -0400

    Added projects

---
+++ LiftBuddy.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LiftBuddy.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
