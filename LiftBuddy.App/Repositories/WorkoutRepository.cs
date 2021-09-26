5b846d67408cb09bbba56624c9eaa24731b77064
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-26 17:45:35 -0400

    Added service and repository

---
+++ LiftBuddy.App/Repositories/WorkoutRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using LiftBuddy.Models;
+
+namespace LiftBuddy.App.Repositories
+{
+    public class WorkoutRepository
+    {
+        public async Task<Workout> CreateAsync(Workout workout)
+        {
+            workout.CreatedAt = DateTime.UtcNow;
+            workout.UpdatedAt = workout.CreatedAt;
+            // TODO: Insert into database
+            return await Task.FromResult(workout);
+        }
+    }
+}
---
+++ LiftBuddy.App/Services/WorkoutService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LiftBuddy.App.Repositories;
+using LiftBuddy.Models;
+
+namespace LiftBuddy.App.Services
+{
+    public class WorkoutService
+    {
+        private readonly WorkoutRepository _workoutRepository;
+        
+        public WorkoutService(WorkoutRepository workoutRepository)
+        {
+            _workoutRepository = workoutRepository;
+        }
+        
+        public async Task<Workout> CreateAsync(string ownerId, Workout workout)
+        {
+            if (workout == null)
+                throw new ArgumentNullException(nameof(workout));
+            
+            workout.OwnerId = ownerId;
+            
+            return await _workoutRepository.CreateAsync(workout);
+        }
+
+        public async Task<Workout> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Workout>> GetAllByOwnerId(string ownerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
