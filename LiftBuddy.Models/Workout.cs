d1dc03ab04aafd0c10adb894e17ca79d9f98ebba
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-26 21:53:04 -0400

    Missed a prop

--- LiftBuddy.Models/Workout.cs
+++ LiftBuddy.Models/Workout.cs
@@ -6,6 +6,7 @@ namespace LiftBuddy.Models
     public class Workout : BaseResource
     {
         public DateTime PerformedAt { get; set; } = DateTime.UtcNow;
+        public int Duration { get; set; }
         public List<Exercise> Exercises { get; set; } = new List<Exercise>();
     }
 }
