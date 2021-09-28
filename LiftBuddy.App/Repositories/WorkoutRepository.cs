4de490b1ad7834126e7357b734fc6d7f840d57e6
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-27 20:13:43 -0400

    Added new methods to service and repo

--- LiftBuddy.App/Repositories/WorkoutRepository.cs
+++ LiftBuddy.App/Repositories/WorkoutRepository.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using LiftBuddy.Models;
 using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace LiftBuddy.App.Repositories
 {
@@ -14,7 +16,6 @@ namespace LiftBuddy.App.Repositories
             _mongoClient = mongoClient;
         }
 
-        
         public async Task<Workout> CreateAsync(Workout workout)
         {
             workout.Id = Guid.NewGuid().ToString();
@@ -22,6 +23,38 @@ namespace LiftBuddy.App.Repositories
             workout.UpdatedAt = workout.CreatedAt;
             await GetCollection().InsertOneAsync(workout);
             return workout;
+        }
+        
+        public async Task<Workout> Update(string id, Workout data)
+        {
+            data.UpdatedAt = DateTime.UtcNow;
+            await GetCollection().ReplaceOneAsync(x => x.Id == id, data);
+            return data;
+        }
+        
+        public async Task Delete(string id)
+        {
+            await GetCollection().UpdateOneAsync(x => x.Id == id,
+                Builders<Workout>.Update.Set(x => x.Deleted, true));
+        }
+
+        public async Task<Workout> GetByIdAsync(string id)
+        {
+            return await GetCollection().AsQueryable()
+                .FirstOrDefaultAsync(w => w.Id == id);
+        }
+
+        public async Task<List<Workout>> GetAll()
+        {
+            return await GetCollection().AsQueryable()
+                .ToListAsync();
+        }
+        
+        public async Task<List<Workout>> GetAllByOwnerId(string ownerId)
+        {
+            return await GetCollection().AsQueryable()
+                .Where(w => w.OwnerId == ownerId && !w.Deleted)
+                .ToListAsync();
         }
         
         private IMongoCollection<Workout> GetCollection()
--- LiftBuddy.App/Services/WorkoutService.cs
+++ LiftBuddy.App/Services/WorkoutService.cs
@@ -25,14 +25,29 @@ namespace LiftBuddy.App.Services
             return await _workoutRepository.CreateAsync(workout);
         }
 
+        public async Task<Workout> Update(string id, Workout workout)
+        {
+            return await _workoutRepository.Update(id, workout);
+        }
+        
+        public async Task Delete(string id)
+        {
+            await _workoutRepository.Delete(id);
+        }
+
         public async Task<Workout> GetById(string id)
         {
-            throw new NotImplementedException();
+            return await _workoutRepository.GetByIdAsync(id);
         }
 
         public async Task<List<Workout>> GetAllByOwnerId(string ownerId)
         {
-            throw new NotImplementedException();
+            return await _workoutRepository.GetAllByOwnerId(ownerId);
+        }
+
+        public async Task<List<Workout>> GetAll()
+        {
+            return await _workoutRepository.GetAll();
         }
     }
 }
