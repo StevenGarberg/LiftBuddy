4c52fc84882a50969bf90b9c64362d4f540ab2a7
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-27 20:13:51 -0400

    Fixed polymorphism bug with Mongo

--- LiftBuddy.Models/Exercise.cs
+++ LiftBuddy.Models/Exercise.cs
@@ -1,5 +1,8 @@
-﻿namespace LiftBuddy.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace LiftBuddy.Models
 {
+    [BsonKnownTypes(typeof(StrengthExercise), typeof(CardioExercise))]
     public abstract class Exercise
     {
         public string Name { get; set; }
