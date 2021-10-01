475d8dbf77da925f6943595efdc2e3e9ec56a62c
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-30 22:48:56 -0400

    Added routine repo and service and implemented logic

---
+++ LiftBuddy.App/Repositories/RoutineRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LiftBuddy.Models;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace LiftBuddy.App.Repositories
+{
+    public class RoutineRepository
+    {
+        private readonly IMongoClient _mongoClient;
+
+        public RoutineRepository(IMongoClient mongoClient)
+        {
+            _mongoClient = mongoClient;
+        }
+
+        public async Task<Routine> CreateAsync(Routine workout)
+        {
+            workout.Id = Guid.NewGuid().ToString();
+            workout.CreatedAt = DateTime.UtcNow;
+            workout.UpdatedAt = workout.CreatedAt;
+            await GetCollection().InsertOneAsync(workout);
+            return workout;
+        }
+        
+        public async Task<Routine> Update(string id, Routine data)
+        {
+            data.UpdatedAt = DateTime.UtcNow;
+            await GetCollection().ReplaceOneAsync(x => x.Id == id, data);
+            return data;
+        }
+        
+        public async Task Delete(string id)
+        {
+            await GetCollection().UpdateOneAsync(x => x.Id == id,
+                Builders<Routine>.Update.Set(x => x.Deleted, true));
+        }
+
+        public async Task<Routine> GetByIdAsync(string id)
+        {
+            return await GetCollection().AsQueryable()
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<Routine>> GetAll()
+        {
+            return await GetCollection().AsQueryable()
+                .ToListAsync();
+        }
+        
+        public async Task<List<Routine>> GetAllByOwnerId(string ownerId)
+        {
+            return await GetCollection().AsQueryable()
+                .Where(x => x.OwnerId == ownerId && !x.Deleted)
+                .ToListAsync();
+        }
+        
+        private IMongoCollection<Routine> GetCollection()
+        {
+            IMongoDatabase database = _mongoClient.GetDatabase("liftbuddy");
+            IMongoCollection<Routine> collection = database.GetCollection<Routine>("routines");
+            return collection;
+        }
+    }
+}
---
+++ LiftBuddy.App/Services/RoutineService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LiftBuddy.App.Repositories;
+using LiftBuddy.Models;
+
+namespace LiftBuddy.App.Services
+{
+    public class RoutineService
+    {
+        private readonly RoutineRepository _routineRepository;
+        
+        public RoutineService(RoutineRepository routineRepository)
+        {
+            _routineRepository = routineRepository;
+        }
+        
+        public async Task<Routine> CreateAsync(string ownerId, Routine routine)
+        {
+            if (routine == null)
+                throw new ArgumentNullException(nameof(routine));
+            
+            routine.OwnerId = ownerId;
+            
+            return await _routineRepository.CreateAsync(routine);
+        }
+
+        public async Task<Routine> Update(string id, Routine routine)
+        {
+            return await _routineRepository.Update(id, routine);
+        }
+        
+        public async Task Delete(string id)
+        {
+            await _routineRepository.Delete(id);
+        }
+
+        public async Task<Routine> GetById(string id)
+        {
+            return await _routineRepository.GetByIdAsync(id);
+        }
+
+        public async Task<List<Routine>> GetAllByOwnerId(string ownerId)
+        {
+            return await _routineRepository.GetAllByOwnerId(ownerId);
+        }
+
+        public async Task<List<Routine>> GetAll()
+        {
+            return await _routineRepository.GetAll();
+        }
+    }
+}
